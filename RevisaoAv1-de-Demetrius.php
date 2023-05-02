9c7345b7f4fbf4775ff835f16a70e417bd40a7a3
Henrique Soares <101074262+GyokuroKunso@users.noreply.github.com>
2023-05-02 11:39:19 -0300

    Add files via upload

---
+++ RevisaoAv1-de-Demetrius.php
@@ -0,0 +1,58 @@
+<?php
+
+//Atividade 1//
+echo ("Atividade 1:<br>");
+echo "<br>";
+
+$values = 5; 
+$soma = 0;
+
+for($i=0; $i<= $values; $i++){
+    $soma += $i;
+
+}
+echo $soma;
+echo "<br>";
+echo "<br>";
+
+//Atividade 2//
+echo ("Atividade 2:<br>");
+$valuis = 5;
+$nome = ("<br>fpb");
+for($i= 0; $i<= $valuis; $i++){
+    $nome .= ("fpb");
+    echo $nome;
+}
+echo "<br>";
+echo "<br>";
+
+//Atividade 3//
+
+echo ("Atividade 3:<br>");
+
+
+$valor1 = 10;
+$valor2 = 3;
+$valor3 = 14;
+
+
+if ( $valor2){ 
+    echo $ordem;
+} else {
+    echo $ordem;
+}
+
+echo $ordem
+
+
+
+
+
+
+
+
+
+
+
+
+?>
