4fdc4fb62cd3cab1e97bf5bce9f2177c4fe848a9
Henrique Soares <101074262+GyokuroKunso@users.noreply.github.com>
2023-05-02 10:53:22 -0300

    Add files via upload

---
+++ RevisaoAv1.php
@@ -0,0 +1,46 @@
+<?php
+
+//Atividade 1//
+echo ("Atividade 1:<br>");
+echo "<br>";
+
+$values = 5; 
+$soma = 0;
+
+for($i=0; $i<= $values; $i++){
+    $soma += $i;
+
+}
+echo $soma;
+echo "<br>";
+echo "<br>";
+
+//Atividade 2//
+echo ("Atividade 2:<br>");
+$valuis = 5;
+$nome = ("<br>fpb");
+for($i= 0; $i<= $valuis; $i++){
+    $nome .= ("fpb");
+    echo $nome;
+}
+echo "<br>";
+echo "<br>";
+
+//Atividade 3//
+
+echo ("Atividade 3:<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
